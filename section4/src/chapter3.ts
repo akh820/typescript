586db3abc141d7aad86515983385cd5e13478ceb
akh820 <akh8202@gmail.com>
2025-08-01 23:46:48 +0900

    feat: 오버로드

---
+++ section4/src/chapter3.ts
@@ -0,0 +1,16 @@
+// 버전들 -> 오버로드 시그니처
+function func(a: number): void;
+function func(a: number, b: number, c: number): void;
+
+// 실제 구현부 -> 구현 시그니처
+function func(a: number, b?: number, c?: number) {
+  if (typeof b === "number" && typeof c === "number") {
+    console.log(a + b + c);
+  } else {
+    console.log(a + 20);
+  }
+}
+
+func(1);
+// func(1, 2);
+func(1, 2, 3);
