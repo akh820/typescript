d7e3968db3915c0e427fe6d534eed0b5a9e507e6
akh820 <akh8202@gmail.com>
2025-08-06 23:41:57 +0900

    feat: ReturnType

---
+++ section10/src/section3.ts
@@ -0,0 +1,17 @@
+type ReturnType<T extends (...args: any) => any> = T extends (
+  ...agrs: any
+) => infer R
+  ? R
+  : never;
+
+function funcA() {
+  return "hello";
+}
+
+function funcB() {
+  return 10;
+}
+
+type ReturnA = ReturnType<typeof funcA>; // string
+// typeof funcA 은 () => string
+type ReturnB = ReturnType<typeof funcB>; // number
